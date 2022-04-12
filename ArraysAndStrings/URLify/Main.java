76c73cf8c8ec119abd436326add654132c3d7b20
JayIssuree <jay_issuree@hotmail.co.uk>
2022-04-12 15:48:57 +0100

    completed URLify problem

---
+++ ArraysAndStrings/URLify/Main.java
@@ -0,0 +1,49 @@
+class Main{
+
+    public static void main(String[] args){
+        String string = "Mr John Smith    ";
+        char[] charArray = string.toCharArray();
+        System.out.println(URLify(charArray, 13));
+        replaceSpaces(charArray, 13);
+        System.out.println(charArray);
+    };
+
+    static String URLify(char[] array, int trueStringLength){
+        String answer = "";
+        for(int i = 0; i < trueStringLength; i++){
+            if(array[i] == ' '){
+                answer += "%20";
+            } else {
+                answer += array[i];
+            };
+        };
+        return answer;
+    };
+
+    static void replaceSpaces(char[] str, int trueLength){
+        int spaceCount = 0;
+        int index;
+        int i = 0;
+        for(i = 0; i < trueLength; i++){
+            if(str[i] == ' '){
+                spaceCount++;
+            };
+        };
+        index = trueLength + spaceCount * 2;
+        if(trueLength < str.length){
+            str[trueLength] = '\0';
+        };
+        for(i = trueLength - 1; i >= 0; i--){
+            if(str[i] == ' '){
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            };
+        };
+    };
+
+}
---
+++ ArraysAndStrings/URLify/README.md
@@ -0,0 +1,25 @@
+## URLify
+### Write a method to replace all spaces ina string with '%20'
+- You may assume that the string has sufficient space at the end to hold the additional characters
+- You are given the 'true' length of the string
+- If implementing in Java, use a character array so that you can perform the operation in place
+
+### Example:
+```
+"Mr John Smith    ", 13 => "Mr%20John%20Smith"
+```
+
+### Questions:
+- Can we use additional space?
+- Do we want to change the original input array?
+
+### Brute Force:
+- Iterate through the string and when the value is blank, substitute in %20
+=> O(n)
+
+### Solution:
+- A common approach is to start from the end of the array/string and work backwards
+=> This allows us to change characters without worrying about what we are overwriting
+- Will need to scan the array twice:
+    - To count the number of spaces
+    - To shuffle each character down and insert %20
